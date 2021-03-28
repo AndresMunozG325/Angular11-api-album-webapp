6f015f7e689f087062470a1308a5df7b1d8fa66c
Andres <andresmunozgo@gmail.com>
2021-03-27 23:41:49 -0300

    commit before netlify

---
+++ src/app/services/photos.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+  albums = []; 
+  constructor(
+    private http: HttpClient
+    ) { }
+
+    getAlbums() {
+      return this.http.get('https://jsonplaceholder.typicode.com/albums')
+    }
+    
+    getPhotos(albumId: any){
+      return this.http.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`);
+
+    }
+}
